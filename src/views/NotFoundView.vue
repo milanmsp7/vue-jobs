747a0c6221647a388838783a575a557a43922ea4
Milan Pithadiya <milanmsp7@gmail.com>
2024-09-28 15:43:05 +0530

    Not found view changes

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,4 +1,31 @@
 <template>
   <h1>404</h1>
   <p>Page not found</p>
+
+  <div>
+    <nav>
+      <router-link to="/">Go to home page</router-link>
+    </nav>
+  <router-view/>
+  </div>
 </template>
+
+<style scoped>
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #0b8a47;
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 4px;
+}
+
+nav a.router-link-exact-active {
+  color: white;
+  background: #42b983;
+}
+</style>
